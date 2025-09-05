3f6669ce54b108ed640ea0eeb4b430c74eb786f0
Umang Gupta <guptaumang2005@gmail.com>
2025-09-05 13:32:58 +0530

    login

--- post_request.py
+++ post_request.py
@@ -1,9 +1,26 @@
-import requests
+from dotenv import load_dotenv
+import os
+import mysql.connector
 
-id="1WPLHhWKe52x4fuiPvRJTjmq6smelBeAP"
-url=f"https://drive.google.com/uc?export=download&id={id}"
-r=requests.get(url)
-with open("img.jpg","wb")as f:
-    f.write(r.content)
+load_dotenv()
+print("USER:", os.getenv("DB_USER"))
+print("PASS:", os.getenv("DB_PASS"))
+print("DB:", os.getenv("DB_DATABASE"))
 
 
+try:
+    con=mysql.connector.connect(
+
+    host="127.0.0.1",
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASS"),
+    database=os.getenv("DB_DATABASE")
+
+)
+    print("Connection succeed")
+
+
+except mysql.connector.Error as e:
+    print("❌ Failed:", e)
+
+    
