3efad04560c551470af29ab93cb92d4e58d790c9
gi19-uma12-g2005 <guptaumang2005@gmail.com>
2025-09-06 13:00:20 +0530

    authen_

--- authentication.py
+++ authentication.py
@@ -1,8 +1,6 @@
 from flask import Flask, render_template, request, jsonify, redirect, url_for
 import mysql.connector
 from werkzeug.security import generate_password_hash, check_password_hash
-import os
-
 app = Flask(__name__, template_folder=r"D:\Umang_Coding\AI_chatbot\template")
 
 #  MySQL connection
