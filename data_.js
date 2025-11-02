9ec8fe12ff8b246567730244da4ea1bb50eaf916
gi19-uma12-g2005 <guptaumang2005@gmail.com>
2025-11-02 11:46:35 +0530

    AI

--- data_.js
+++
@@ -1,7 +0,0 @@
-const response = await fetch("http://127.0.0.1:8000/api/chat", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ message: text, language: language })
-});
-const data = await response.json();
-appendMessage("bot", data.reply || "Sorry, I didn’t get that.");
